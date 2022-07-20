a7c1d5257ffb245457d8945a2a073883a1dbebff
Suhel-Kap <suhelkapadia2@gmail.com>
2022-07-20 18:48:49 +0530

    corrected file

--- components/LotteryEntrance.jsx
+++ components/LotteryEntrance.jsx
@@ -99,7 +99,7 @@ export default function LotteryEntrace() {
                                 <svg
                                     aria-hidden="true"
                                     role="status"
-                                    class="inline mr-3 w-4 h-4 text-white animate-spin"
+                                    className="inline mr-3 w-4 h-4 text-white animate-spin"
                                     viewBox="0 0 100 101"
                                     fill="none"
                                     xmlns="http://www.w3.org/2000/svg"
